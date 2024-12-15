ebe01cb88469b95e8d6bd1dbd53570f02a151b59
Martín Peñaloza <mpenaloza@uc.cl>
2024-12-15 04:07:43 -0300

    feat: Added two endpoints for wishlists get

--- octopus/db/schema.rb
+++ octopus/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_15_033031) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_15_065227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_15_033031) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth0_id"
   end
 
   create_table "wishlists", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
