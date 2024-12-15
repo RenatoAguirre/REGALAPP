25d89ed08bcf2cab7295fd5b9b2bc2848f8b3f12
RenatoAguirre <raaguirre1@miuandes.cl>
2024-12-15 00:13:28 -0300

    feat: add validations and association for Product model

--- octopus/app/models/product.rb
+++ octopus/app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  belongs_to :wishlist
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :bought, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+  validates :link, presence: true
+  validates :wishlist_id, presence: true
 end
